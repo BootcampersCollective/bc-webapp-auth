34726be77296da97262b5109ebe50ed68e0a981b
Trenton Greer <yoshigreer@gmail.com>
2017-10-28 12:44:35 -0600

    added auth.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 var express = require ('express');
 
 var app = express ();
+const port = 3030;
 
-app.listen (3030, () => {
-  console.log('server is up on port 3030');
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get('/api/google_oauth', function(req, res){
+  res.send('Got a GET request at `api/google_oath');
+});
+
+app.listen (port, () => {
+  console.log('server is up on port' + port);
 });
